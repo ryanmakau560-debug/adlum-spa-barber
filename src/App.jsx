563ffde86208f28fe0808a78e5cade0a016580b0
Ryan <ryanmakau560@gmail.com>
2026-04-13 02:00:00 +0300

    Fixing case sensitivity for Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/Footer.jsx';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import History from './pages/History';
